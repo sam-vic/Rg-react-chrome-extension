3590ef63aae7ea1056e2cdd4b8cd9784979dfde1
Samuel Lu <samuel-lu@hotmail.com>
2020-09-04 12:24:16 -0700

    Header fix

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,10 +4,8 @@ import style from './Header.css';
 export default class Header extends Component {
   render() {
     return (
-      <header>
-        <imput type="file"/>
-        <img src={require('../images/logo.png').default} alt='logo' className={style.logo} />
-        <div className={style.title}>Reelgood</div>
+      <header className={style.page}>
+        <img src={require('../images/logo.png').default} alt="logo" className={style.logo} />
       </header>
     );
   }
